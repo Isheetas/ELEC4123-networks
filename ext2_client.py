d0b388a002f8095700c9d24ab4ccc0abeb4cfba4
christineazer <31399238+christineazer@users.noreply.github.com>
2020-11-20 17:00:35 +1100

    Add files via upload
    final

--- ext2_client.py
+++ ext2_client.py
@@ -1,5 +1,28 @@
 #client for extension task 1
 import socket
+def decrypt_rsa(cipher_bytes, n, d):
+    # message to decrypt will always be a multiple of 16
+    plaintext_bytes_temp = b''
+    plaintext_bytes = b''
+    start = 0
+    while start < len(cipher_bytes):
+        end = start + 16
+        cipher_int = int.from_bytes(cipher_bytes[start:end], byteorder='big') # convert cipher to integer form
+        plaintext_int = pow(cipher_int, int(d), int(n))
+        plaintext_bytes_temp = int_to_bytes(plaintext_int)
+        plaintext_bytes = plaintext_bytes + plaintext_bytes_temp
+        start = end
+    return plaintext_bytes
+
+
+def int_to_bytes(integer_val):
+    '''
+    input: int
+    output: list of bytes(binary)
+    '''
+    return integer_val.to_bytes((integer_val.bit_length() + 7) // 8, 'big')
+
+
 
 #send request to task3_server
 HOST = '127.0.0.1'
@@ -8,6 +31,8 @@ PORT = 3000
 
 #request = 'GET / HTTP/1.1\r\nHost: ' + host + '\r\nContent-Length: ' + str(len(content)) + '\r\n\r\n' + content
 #client sends 'n,e'
+n = '252837207378338387332619197259204540353'
+d = '48393883292703003300067554859838128129'
 request = '252837207378338387332619197259204540353,65537'
 request_bytes = bytes(request, 'utf-8')
 try:
@@ -22,8 +47,9 @@ s.send(request_bytes)
 data = s.recv(1024)
 s.close()
 print('Received', repr(data))
-#correct bit errors
 
-#remove error correcting codes
+#decrypt data
+plaintext = decrypt_rsa(data, n, d)
 
 #display data
+print(plaintext)
--- ext2_server.py
+++ ext2_server.py
@@ -1,81 +1,6 @@
 # create server and client
 import socket
 import select
-
-
-def main():
-
-    #CREATE SERVER
-    HOST = '127.0.0.1'
-    PORT = 3000
-    HOST_server = '149.171.36.192'
-    PORT_d = 12000
-
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    s.bind((HOST, PORT))
-    s.listen(5)
-
-    conn, addr = s.accept()
-    print(f"Connection from {addr} has been established.")
-
-    while True:
-
-        try:
-            data = conn.recv(1024)
-
-            if not data: break
-            print("Client says: ")
-            print(data)
-            conn.close()
-
-        except socket.error:
-            print ("Error Occured.")
-            break
-
-
-
-    #Request message from data server
-    content = '4,3'
-    request_bytes =  bytes(content, 'utf-8')
-
-
-    try:
-        s_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print ("Socket successfully created to data server")
-    except socket.error as err:
-        print ("socket creation failed with error %s to data server" %(err))
-
-    s_data.connect((HOST_server, PORT_d))
-    s_data.send (request_bytes)
-
-    #s.send (request_bytes)
-    ready = select.select([s_data], [], [], 10)
-    if ready[0]:
-        response = s_data.recv(64000)
-
-    s_data.close()
-
-    print(response)
-
-    #encrypt with rsa
-    #payload = data.split('b')
-
-
-    payload = str(data)
-    payload = payload[2:len(payload)-1]
-    payload_split = payload.split(',')
-    n = int(payload_split[0])
-    print(n)
-    e = int(payload_split[1])
-    print(e)
-    cipher = encrypt_rsa(response, n, e)    
-    print(response)
-
-    #send noisy payload to client
-    conn.sendall(response)
-
 def encrypt_rsa(msg_bytes, n, e):
     '''
     Encrypt with RSA
@@ -96,6 +21,21 @@ def encrypt_rsa(msg_bytes, n, e):
 
 
 
+def decrypt_rsa(cipher_bytes, n, d):
+    # message to decrypt will always be a multiple of 16
+    plaintext_bytes_temp = b''
+    plaintext_bytes = b''
+    start = 0
+    while start < len(cipher_bytes):
+        end = start + 16
+        cipher_int = int.from_bytes(cipher_bytes[start:end], byteorder='big') # convert cipher to integer form
+        plaintext_int = pow(cipher_int, int(d), int(n))
+        plaintext_bytes_temp = int_to_bytes(plaintext_int)
+        plaintext_bytes = plaintext_bytes + plaintext_bytes_temp
+        start = end
+    return plaintext_bytes
+
+
 def int_to_bytes(integer_val):
     '''
     input: int
@@ -103,6 +43,79 @@ def int_to_bytes(integer_val):
     '''
     return integer_val.to_bytes((integer_val.bit_length() + 7) // 8, 'big')
 
+#CREATE SERVER
+HOST = '127.0.0.1'
+PORT = 3000
+HOST_server = '149.171.36.192'
+PORT_d = 12000
 
-if __name__ == '__main__':
-    main()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((HOST, PORT))
+s.listen(5)
+
+conn, addr = s.accept()
+print(f"Connection from {addr} has been established.")
+
+# while True:
+#
+#     try:
+#         data = conn.recv(1024)
+#
+#         if not data: break
+#         print("Client says: ")
+#         print(data)
+#         #conn.close()
+#
+#     except socket.error:
+#         print ("Error Occured.")
+#         break
+
+data = conn.recv(1024)
+
+#Request message from data server
+content = '4,3'
+request_bytes =  bytes(content, 'utf-8')
+
+try:
+    s_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print ("Socket successfully created to data server")
+except socket.error as err:
+    print ("socket creation failed with error %s to data server" %(err))
+
+s_data.connect((HOST_server, PORT_d))
+s_data.send (request_bytes)
+
+#s.send (request_bytes)
+ready = select.select([s_data], [], [], 10)
+if ready[0]:
+    response = s_data.recv(64000)
+
+s_data.close()
+
+print(response)
+
+#encrypt with rsa
+
+# data = str(data)
+# #payload = data.split('b')
+# payload_split = data.split(',')
+# n = int(payload_split[0])
+# e = int(payload_split[1])
+# encrypt_rsa(response, n, e)
+
+payload = str(data)
+payload = payload[2:len(payload)-1]
+payload_split = payload.split(',')
+n = int(payload_split[0])
+print(n)
+e = int(payload_split[1])
+print(e)
+cipher = encrypt_rsa(response, n, e)
+print(cipher)
+
+#send noisy payload to client
+conn.sendall(cipher)
+
+
