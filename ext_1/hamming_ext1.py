e784943cd6056711f13f292995bc9161fd6230a1
Isheeta Sinha <i.sinha@unsw.edu.au>
2020-11-22 23:28:30 +1100

    extension 3

--- ext_1/hamming_ext1.py
+++ ext_1/hamming_ext1.py
@@ -1,21 +1,29 @@
 import math
 from type_conversions import *
 
+import math
+from type_conversions import *
 
-def hamming_decode(input, extraparity):
+
+
+def hamming_decode (input, extraparity):
     '''
     input: binary string of 1s and 0s
     output: binary string of 1s and 0s - with parity bits removed
     '''
 
-    # input = '011100101110'         #test input
-
+    #input = '011100101110'         #test input
+    count = 0
+    i=0
     # caluclate number of parity bits
-    #print(input)
-    if extraparity:
-        #input = bytes_to_bits(input)
-        extrabit = int(input[0])
-        input = input[1:]
+    if (extraparity):
+        while (i<len(input)):
+            if input[i]=='1':
+                count = count+1
+            i=i+1
+    
+        overallparity = count%2
+        input = input [1:]
     '''
     else:
         input = bytes_to_bits(input)
@@ -23,10 +31,9 @@ def hamming_decode(input, extraparity):
     '''
     n = len(input)
     numParity = 0
-    while 2 ** numParity < n + 1:
+    while 2**numParity < n + 1:
         numParity = numParity + 1
-    data = input[::-1]  # flip data
-    #print(data)
+    data = input[::-1]          #flip data 
     output = list(data)
     errorDetect = []
 
@@ -42,10 +49,7 @@ def hamming_decode(input, extraparity):
 
             while (cluster < parity and start + cluster < len(data)):
                 valueparity = int(data[start + cluster])
-                Sum = Sum + valueparity
-                if extraparity:
-                    if extrabit == 0 and valueparity != 0:
-                        return 'resend'
+                Sum = Sum + valueparity               
                 dataBits.append(start + cluster)
                 cluster = cluster + 1
 
@@ -57,13 +61,17 @@ def hamming_decode(input, extraparity):
     oddPar = []
     for x in errorDetect:
         if x == 1:
-            oddPar.append(2 ** p)
+            oddPar.append(2**p)
         p = p + 1
 
-    index = sum(oddPar)
 
+    index = sum(oddPar)
+    if (extraparity):
+        #print ('oddPar', oddPar)
+        if overallparity == 0 and index != 0:
+            return 'resend' 
     #print("index:", index)
-    #print(len(data))
+
     if index > 0 and data[index - 1] == '1':
         output[index - 1] = str(0)
     elif index > 0:
@@ -73,12 +81,14 @@ def hamming_decode(input, extraparity):
     # remove parity bits
     r = numParity - 1
     while r >= 0:
-        par = 2 ** r
-        output.pop(par - 1)
-        r = r - 1
+        par = 2**r
+        output.pop(par-1)
+        r = r-1
+
 
     output = output[::-1]
-    output = [str(x) for x in output]
+  
+
     # convert list output to string
     strOutput = ""
     str_bin = strOutput.join(output)
@@ -87,39 +97,41 @@ def hamming_decode(input, extraparity):
 
 
 def hamming_encode(msg_byte, extraparity):
-    # HAMMING ENCODER
-    # takes in data bits, outputs data bits and parity bits
-    # no error detection or correction required
-    # even parity
-    if extraparity:
-        data = msg_byte
+    #HAMMING ENCODER
+    #takes in data bits, outputs data bits and parity bits
+    #no error detection or correction required
+    #even parity
+    #if (extraparity):
+    data = msg_byte
+    #else:
+    #    msg_int = int.from_bytes(msg_byte, byteorder='big')
+    #    msg_bin = bin(msg_int)
 
-    else:
-        msg_int = int.from_bytes(msg_byte, byteorder='big')
-        msg_bin = bin(msg_int)
+    #    data = msg_bin[2:]
 
-        data = msg_bin[2:]
-
-    # print("before encoding:", int(data,2), len(data))
-    # print("before:", int(data,2))
+    #print("before encoding:", int(data,2), len(data))
+    #print("before:", int(data,2))
 
     data = data[::-1]
     output = list(data)
-    #print(output)
 
-    # calculate how many parity bits are required
+    #calculate how many parity bits are required
     m = len(data)
     r = 0
-    while 2 ** r < m + r + 1:
+    while 2**r < m + r + 1:
         r = r + 1
+    #print(r)
 
-    # print ("r", r)
-    # insert parity bits in correct positions w value 0
+    
+
+    #print ("r", r)
+    #insert parity bits in correct positions w value 0
 
     for n in range(0, r):
-        output.insert((2 ** n) - 1, str(0))
+        output.insert((2**n) - 1, str(0))
 
-    # calculate parity bit values
+    #print("".join(output))
+    #calculate parity bit values
     for i in range(r):
         parity = 2 ** i  # 2^0, 2^1, 2^2, ...
         pos = parity - 1
@@ -137,37 +149,33 @@ def hamming_encode(msg_byte, extraparity):
 
             start = start + 2 * parity
 
-        # if odd parity
+        #if odd parity
         if sum % 2 == 1:
-            output[(2 ** i) - 1] = str(1)
+            output[(2**i) - 1] = str(1)
+        #print("output:", output)
 
-    # convert list to string
-    #print(output)
-    # strOutput = ""
-    # out = strOutput.join(output)
-
-    #out = [str(x) for x in output]
-    out = output
-    #print(out)
-
-    if extraparity:
-        i = 0
-        countones = 0
-        while (i < len(out)):
-            if out[i] == '1':
-                countones = countones + 1
-            i = i + 1
-
-        if (countones % 2):
-            out.append('1')
-        else:
-            out.append('0')
-    #convert list of strings to string
+    #convert list to string
     strOutput = ""
-    out = strOutput.join(out)
-    # flip back to correct way
-    out = out[::-1]
-    encoded_bytes = bits_to_bytes(out)
+    out = strOutput.join(output)
+    
+    #if (extraparity):
+    i=0
+    countones = 0
+    while (i<len(out)):
+        if out[i]=='1':
+            countones = countones+1
+        i=i+1
+    
+    if (countones%2):
+        out = out+'1'
+    else:
+        out = out+'0'
+    #flip back to correct way
+    
+    out = out [::-1]
+    print ('out', out)
+    return out
+#1000111
 
-    #encoded_bytes = encoded_bytes[::-1]
-    return encoded_bytes
+#1101001
+
--- ext_1/task3_client.py
+++ ext_1/task3_client.py
@@ -2,48 +2,15 @@
 import socket
 from hamming_ext1 import *
 
-#send request to task3_server
-HOST = '127.0.0.1'
-PORT = 3000
-#host = 'localhost'
 
-#request = 'GET / HTTP/1.1\r\nHost: ' + host + '\r\nContent-Length: ' + str(len(content)) + '\r\n\r\n' + content
-request = 'thisisarequest'
-request_bytes = bytes(request, 'utf-8')
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket successfully created")
-except socket.error as err:
-    print("socket creation failed with error %s" % (err))
+def main():
+    #send request to task3_server
+    HOST = '127.0.0.1'
+    PORT = 3000
+    #host = 'localhost'
 
-s.connect((HOST, PORT))
-s.send(request_bytes)
-
-data = s.recv(1024)
-#s.close()
-print('Received', repr(data))
-#convert from bytes to bits
-data = bytes_to_bits(data)
-#print(data)
-#split received data into blocks of size 8 bits
-n = 8
-splitresponse = [data[i:i + n] for i in range(0, len(data), n)]
-#print(splitresponse)
-#correct bit errors
-hammingsplit = []
-resendFlag = 0
-for x in splitresponse:
-    binDecoded = hamming_decode(x, 1)
-    hammingsplit.append(binDecoded)
-    if binDecoded == 'resend':
-        print(x)
-        print("NEED TO RESEND")
-        resendFlag = 1
-
-#print(hammingsplit)
-if resendFlag == 1:
-    request_again = 'resend'
-    request_again_bytes = bytes(request_again, 'utf-8')
+    request = 'request'
+    request_bytes = bytes(request, 'utf-8')
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("Socket successfully created")
@@ -51,26 +18,84 @@ if resendFlag == 1:
         print("socket creation failed with error %s" % (err))
 
     s.connect((HOST, PORT))
-    s.send(request_again_bytes)
+    s.send(request_bytes)
 
-    data_again = s.recv(1024)
-    s.close()
-    print('Received', repr(data_again)) #same data w new noise
-    data_again = bytes_to_bits(data_again)
-    splitresponse_again_ = [data_again[i:i + n] for i in range(0, len(data_again), n)]
-    hammingsplit_again = []
-    #assuming won't need to be resent a third time
-    for x in splitresponse_again_:
+    data = s.recv(1024)
+    #s.close()
+    print('Received', repr(data))
+    #convert from bytes to bits
+    data = bytes_to_bits(data)
+    #print(data)
+    #split received data into blocks of size 8 bits
+    n = 8
+    splitresponse = [data[i:i + n] for i in range(0, len(data), n)]
+    #print(splitresponse)
+    #correct bit errors
+    hammingsplit = []
+    resendFlag = 0
+    for x in splitresponse:
         binDecoded = hamming_decode(x, 1)
-        hammingsplit_again.append(binDecoded)
+        print("bin_decodd:", x, binDecoded)
+
+        hammingsplit.append(binDecoded)
+        if binDecoded == 'resend':
+            #print(x)
+            #print("NEED TO RESEND")
+            resendFlag = 1
+
+    s.close
+
+    #print(hammingsplit)
+    if resendFlag == 1:
+        request_again = 'resend'
+        request_again_bytes = bytes(request_again, 'utf-8')
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Socket successfully created")
+        except socket.error as err:
+            print("socket creation failed with error %s" % (err))
+
+        s.connect((HOST, PORT))
+        s.send(request_again_bytes)
+
+        data_again = s.recv(1024)
+        s.close()
+        print('Received', repr(data_again)) #same data w new noise
+        data_again = bytes_to_bits(data_again)
+        splitresponse_again_ = [data_again[i:i + n] for i in range(0, len(data_again), n)]
+        hammingsplit_again = []
+        #assuming won't need to be resent a third time
+        for x in splitresponse_again_:
+            binDecoded = hamming_decode(x, 1)
+            print("bin_decodd:", x, binDecoded)
+
+            hammingsplit_again.append(binDecoded)
+
+        print("hamming_decoded 1:", hammingsplit)
+
+        print("hamming_decoded 2:", hammingsplit_again)
+
 
     #compare both received messages
-s.close()
-strOutput = ""
-out = strOutput.join(hammingsplit)
+    final = []
+    index = 0
+    for i in hammingsplit:
+        if i == "resend" and hammingsplit_again[index] != "resend":
+            final.append(hammingsplit_again[index])
+        else:
+            final.append(i)
+        index +=1
 
-decoded = bits_to_bytes(out)
-#decoded = hamming_decode(data, 1)
-#decoded = bits_to_bytes(decoded)
-#display data
-print(decoded)
+    print("final:", final)
+    
+    if "resend" not in final:
+        bin = "".join(final)
+        print("msg:", bits_to_bytes(bin))
+
+    else:
+        print("resend in both")
+    s.close()
+    
+
+if __name__ == '__main__':
+    main()
